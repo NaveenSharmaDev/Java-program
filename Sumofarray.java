faeed53dfa5f9a82cbd022378e193e328d7adb50
Naveen Sharma <naveensharmaaurangabad@gmail.com>
2026-08-05 23:44:42 +0530

    Format Sumofarray.java for better readability

--- Sumofarray.java
+++ Sumofarray.java
@@ -1,19 +1,30 @@
 import java.util.Scanner;
+
 class Sumofarray
+  
 {
+  
 public static void main(String arg[])
+  
 {
+  
 int arr[]=new int[] {1,2,3,4,5};
 int sum=0;
 Scanner sc=new Scanner(System.in);
 System.out.println("Enter elements num");
+  
 
 for(int i=0; i<5; i++)
+  
 {
+  
 arr[i]=sc.nextInt();
 
 sum=arr[i]+sum;
+  
 }
 System.out.println("\n sum of all element "+sum);
+  
 }
+  
 }  
