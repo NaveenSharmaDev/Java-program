f8f9de7184abe987b3948b8557822f5a5fa69f6b
Naveen Sharma <naveensharmaaurangabad@gmail.com>
2026-08-05 23:43:31 +0530

    Format Swap.java for better readability

--- Swap.java
+++ Swap.java
@@ -1,15 +1,26 @@
 import java.util.Scanner;
+
 class Swap
+  
 {
+  
 public static void main(String arg[])
+  
 {
+  
 Scanner sc=new Scanner(System.in);
+  
 int a,b;
+  
 a=sc.nextInt();
+  
 b=sc.nextInt();
 a=a+b;
 b=a-b;
 a=a-b;
+  
 System.out.println("\n after Swap="+a +" " +b);
+  
 }
+  
 }
